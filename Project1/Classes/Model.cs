a66e3e00a03624f12eb186a7c2361b7974e92b20
Owen Aderanti <S00163169@mail.itsligo.ie>
2017-11-17 00:45:48 +0000

    Add project files.

---
+++ Project1/Classes/Model.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net.Mail;
+
+namespace Project1
+{
+    public class Model
+    {
+        #region Properties
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string UserID { get; set; }
+        #endregion
+
+        #region Constructors
+        public Model()
+        {
+        }
+
+        public Model(string firstname, string surname, string email, string password, string phoneNumber) : this()
+        {
+            this.Firstname = firstname;
+            this.Surname = surname;
+            this.Email = email;
+            this.Password = password;
+            this.Phone = phoneNumber;
+        }
+        #endregion
+    }
+
+    public class Product
+    {
+        #region Properties
+        public string ImageUrl { get; set; }
+        public string ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        #endregion
+
+        #region Constructors
+        public Product()
+        {
+
+        }
+
+        public Product(string imageUrl, string productID, decimal price, string name) : this()
+        {
+            this.ProductID = productID;
+            this.Name = name;
+            this.Price = price;
+            this.ImageUrl = imageUrl;
+        }
+        #endregion
+    }
+
+    public class DatabaseControl
+    {
+        static public void AddBasketItem()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
+
+            SqlConnection db = new SqlConnection(connectionString);
+
+            SqlCommand cmd = new SqlCommand("AddToBasket", db);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter inputUserID = cmd.Parameters.Add("@UserID", SqlDbType.NVarChar, 255);
+            inputUserID.Direction = ParameterDirection.Input;
+
+            SqlParameter inputProductID = cmd.Parameters.Add("@ProductID", SqlDbType.NVarChar, 255);
+            inputProductID.Direction = ParameterDirection.Input;
+
+            SqlParameter inputQuantity = cmd.Parameters.Add("@Quantity", SqlDbType.Int);
+            inputQuantity.Direction = ParameterDirection.Input;
+
+            //string UserId = Version["UserID"].ToString();
+
+
+            inputUserID.Value = "";
+            inputProductID.Value = "";
+            inputQuantity.Value = 1;
+        }
+
+        static public List<Product> ReturnProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            using (SqlConnection boothtest = new SqlConnection("Server=tcp:boothserver.database.windows.net,1433;" +
+                "Initial Catalog=boothtest;Persist Security Info=False;" +
+                "User ID=S00163774;Password=BOOTHserver%163774;" +
+                "MultipleActiveResultSets=False;Encrypt=True;" +
+                "TrustServerCertificate=False;Connection Timeout=30;"))
+            {
+                using (SqlCommand ProductsCMD = new SqlCommand("ReturnProducts", boothtest))
+                {
+                    ProductsCMD.CommandType = CommandType.StoredProcedure;
+
+                    boothtest.Open();
+
+                    SqlDataReader myReader = ProductsCMD.ExecuteReader();
+
+                    products = new List<Product>();
+
+                    while (myReader.Read())
+                    {
+                        Product product = new Product();
+
+                        product.ProductID = myReader["ProductID"].ToString();
+                        product.Name = myReader["ProductName"].ToString();
+                        //product.ProductDesc = myReader["ProductDesc"].ToString();
+                        product.Price = decimal.Parse(myReader["ProductPrice"].ToString());
+                        //product.ProductStock = int.Parse(myReader["ProductStock"].ToString());
+                        product.ImageUrl = myReader["ProductURL"].ToString();
+                        //product.ProductCategoryID = int.Parse(myReader["ProductCategoryID"].ToString());
+
+                        products.Add(product);
+                    }
+
+                    myReader.Close();
+                    boothtest.Close();
+
+                    return products;
+                }
+            }
+        }
+
+        //static public User LogIn(string email, string password)
+        //{
+        //    User user = new User();
+
+        //    using (SqlConnection boothtest = new SqlConnection("Server=tcp:boothserver.database.windows.net,1433;" +
+        //        "Initial Catalog=boothtest;Persist Security Info=False;" +
+        //        "User ID=S00163774;Password=BOOTHserver%163774;" +
+        //        "MultipleActiveResultSets=False;Encrypt=True;" +
+        //        "TrustServerCertificate=False;Connection Timeout=30;"))
+        //    {
+        //        using (SqlCommand LogInCMD = new SqlCommand("LogIn", boothtest)) // Create New Command calling 'LogIn' stored procedure in boothtest database
+        //        {
+        //            LogInCMD.CommandType = CommandType.StoredProcedure;
+
+        //            // Input Variables
+        //            SqlParameter inputEmail = LogInCMD.Parameters.Add("@ExUserEmail", SqlDbType.NVarChar, 30);
+        //            inputEmail.Direction = ParameterDirection.Input;
+
+        //            SqlParameter inputPassword = LogInCMD.Parameters.Add("@ExuserPassword", SqlDbType.NVarChar, 30);
+        //            inputPassword.Direction = ParameterDirection.Input;
+
+        //            inputEmail.Value = email;
+        //            inputPassword.Value = password;
+
+        //            boothtest.Open(); // Open Database Connection
+
+        //            SqlDataReader myReader = LogInCMD.ExecuteReader(); // Execute Command
+
+        //            while (myReader.Read())
+        //            {
+        //                user.UserID = int.Parse(myReader["UserID"].ToString());
+        //                user.UserFirstName = myReader["UserFirstName"].ToString();
+        //                user.UserSurname = myReader["UserSurname"].ToString();
+        //                user.UserMobileNumber = myReader["UserMobileNumber"].ToString();
+        //                user.UserEmail = myReader["UserEmail"].ToString();
+        //                user.UserType = myReader["UserType"].ToString();
+        //            }
+
+        //            myReader.Close(); // Close Command
+
+        //            boothtest.Close(); // Close Database Connection
+        //        }
+        //    }
+
+        //    return user;
+        //}
+
+        //static public string Register(string firstname, string surname, string mobilenumber, string email, string password)
+        //{
+        //    string emailExists = null;
+
+        //    using (SqlConnection boothtest = new SqlConnection("Server=tcp:boothserver.database.windows.net,1433;" +
+        //        "Initial Catalog=boothtest;Persist Security Info=False;" +
+        //        "User ID=S00163774;Password=BOOTHserver%163774;" +
+        //        "MultipleActiveResultSets=False;Encrypt=True;" +
+        //        "TrustServerCertificate=False;Connection Timeout=30;"))
+        //    {
+        //        using (SqlCommand RegisterCMD = new SqlCommand("Register", boothtest))
+        //        {
+        //            RegisterCMD.CommandType = CommandType.StoredProcedure;
+
+        //            // Input Variables
+        //            SqlParameter inputFirstName = RegisterCMD.Parameters.Add("@UserFirstName", SqlDbType.NVarChar, 20);
+        //            inputFirstName.Direction = ParameterDirection.Input;
+
+        //            SqlParameter inputSurname = RegisterCMD.Parameters.Add("@UserSurname", SqlDbType.NVarChar, 30);
+        //            inputSurname.Direction = ParameterDirection.Input;
+
+        //            SqlParameter inputMobileNumber = RegisterCMD.Parameters.Add("@UserMobileNumber", SqlDbType.NVarChar, 10);
+        //            inputMobileNumber.Direction = ParameterDirection.Input;
+
+        //            SqlParameter inputEmail = RegisterCMD.Parameters.Add("@UserEmail", SqlDbType.NVarChar, 50);
+        //            inputEmail.Direction = ParameterDirection.Input;
+
+        //            SqlParameter inputPassword = RegisterCMD.Parameters.Add("@UserPassword", SqlDbType.VarChar, 50);
+        //            inputPassword.Direction = ParameterDirection.Input;
+
+        //            SqlParameter returnEmailExists = RegisterCMD.Parameters.Add("Result", SqlDbType.Int);
+        //            returnEmailExists.Direction = ParameterDirection.ReturnValue;
+
+        //            inputFirstName.Value = firstname;
+        //            inputSurname.Value = surname;
+        //            inputMobileNumber.Value = mobilenumber;
+        //            inputEmail.Value = email;
+        //            inputPassword.Value = password;
+
+        //            boothtest.Open(); // Open Database Connection
+
+        //            SqlDataReader myReader = RegisterCMD.ExecuteReader(); // Execute Command
+
+        //            emailExists = Convert.ToString(returnEmailExists.Value);
+
+        //            myReader.Close(); // Close Command
+
+        //            boothtest.Close(); // Close Database Connection
+        //        }
+
+        //        return emailExists;
+        //    }
+        //}
+    }
+
+    public class Basket
+    {
+        static List<Product> basketProducts = new List<Product>();
+
+        static public void AddToBasket(string productId)
+        {
+            List<Product> dbProducts = DatabaseControl.ReturnProducts();
+
+            foreach (Product prod in dbProducts)
+            {
+                if (prod.ProductID == productId)
+                {
+                    bool check = BasketChecker(prod);
+
+                    if (!check)
+                    {
+                        basketProducts.Add(prod);
+                    }
+                }
+            }
+        }
+
+        static bool BasketChecker(Product product)
+        {
+            if (basketProducts.Contains(product))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Checkout
+    {
+
+    }
+}
---
+++ Project1/js/programmerDefined.js
@@ -0,0 +1,86 @@
+﻿'use strict';
+
+(function EventListeners() {
+    // Select all buttons on the products 
+    var plus = document.getElementsByName("pQuantity");
+    var minus = document.getElementsByName("mQuantity");
+    var basket = document.getElementsByName("addToBasket");
+
+    var plusValue = 0;
+    var minusValue = 0;
+    var basketValue = 0;
+    // add id to all 'plus quantity' buttons
+    for (var i = 0; i < plus.length; i++) {
+        plus[i].setAttribute("id", "P" + plusValue);
+        plusValue++;
+    }
+    // add id to all 'minus quantity' buttons
+    for (var j = 0; j < minus.length; j++) {
+        minus[j].setAttribute("id", "M" + minusValue);
+        minusValue++;
+    }
+    // add id to all 'add to basket' buttons
+    for (var k = 0; k < minus.length; k++) {
+        basket[k].setAttribute("id", "B" + basketValue);
+        basketValue++;
+    }
+})();
+
+function minusQuantity(clicked_id) {
+    // select product id and quantity
+    var pos = clicked_id.charAt(clicked_id.length - 1);
+    var number = parseInt($('span.badge')[pos].innerText);
+    // decrease the quantity and show result
+    if (number <= 1) {
+        number = 1;
+    }
+    else {
+        number -= 1;
+    }
+
+    $('span.badge')[pos].innerText = number;
+}
+
+function plusQuantity(clicked_id) {
+    // select product id and quantity
+    var pos = clicked_id.charAt(clicked_id.length - 1);
+    var number = parseInt($('span.badge')[pos].innerText);
+    // increase the quantity and show result
+    if (number >= 10) {
+        number = 10;
+    }
+    else {
+        number += 1;
+    }
+
+    $('span.badge')[pos].innerText = number;
+}
+
+function AddToBasket(clicked_id, productID) {
+    // select quantity and product id
+    var pos = clicked_id.charAt(clicked_id.length - 1);
+    var quantity = $('span.badge')[pos].innerText;
+    // place the product and quantity into session storage
+    if (quantity != 0 || quantity != null) {
+        var basketItem = { 'product': productID, 'quantity': quantity };
+        sessionStorage.setItem('basketItem' + productID, JSON.stringify(basketItem));
+
+        toastr["success"]('Added to basket!');
+        toastr.options = { "showMethod": "fadeIn", "hideMethod": "fadeOut", "timeOut": "3000", "closeButton": true };
+    }
+}
+
+function DisplayBasket() {
+    // collect all the keys in storage
+    var keys = [];
+    var allKeys = "";
+    
+
+    for (var i = 0; i < sessionStorage.length; i++) {
+        keys[i] = sessionStorage.key(i);
+    }
+
+    sessionStorage.setItem("allKeys", JSON.stringify(keys));
+
+    window.location.href = "cart.cshtml";
+}
